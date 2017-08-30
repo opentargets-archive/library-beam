5731e81642fb42215474788e7f37800e97bcaac8
Priyanka Wankar <priyanka.wankar@biogen.com>
2017-08-29 20:44:04 -0400

    Changes to tag hyphenated common words correctly

--- tests/test_tagger.py
+++ tests/test_tagger.py
@@ -1,10 +1,39 @@
+import unittest, json
 
 from modules.BioentityTagger import BioEntityTagger
 
 
-tagger = BioEntityTagger()
+class TaggerTestCase(unittest.TestCase):
 
-for i, text in enumerate(file('resources/test_abstract_nlp.txt')):
-    print i
-    for tag in tagger.tag(text.lower()):
-        print tag, text[tag['start']:tag['end']]
+    def setUp(self):
+        self.tagger = BioEntityTagger()
+
+    def testTaggerNLP(self):
+
+        for i, text in enumerate(file('resources/test_abstract_nlp.txt')):
+            print i
+            for tag in self.tagger.tag(text.lower()):
+               print tag, text[tag['start']:tag['end']]
+
+    def testTaggerLexebi(self):
+        for i, text in enumerate(file('resources/test_abstract_lexebi.txt')):
+
+            print i
+            # for tag in tagger.tag(text.lower()):
+            #     print tag, text[tag['start']:tag['end']]
+            old_tags = set()
+            lexebi_tags = set()
+            tags = self.tagger.tag(text.lower())
+            for tag in tags:
+                matched_text = text[tag['start']:tag['end']]
+                print tag, matched_text
+                if tag['reference_db'] == 'LEXEBI':
+                    lexebi_tags.add(matched_text)
+                else:
+                    old_tags.add(matched_text)
+            new_tags = lexebi_tags.difference(old_tags)
+            print 'New tags identified : {}'.format(new_tags)
+
+
+if __name__ == "__main__":
+     unittest.main()
