76dee59c99a8d0839f975f9db75bb7680d47cc66
Cinzia Malangone <cmalangone@users.noreply.github.com>
2019-12-02 13:37:31 +0000

    update permissions

--- gcp/exec.sh
+++ gcp/exec.sh
@@ -1,10 +1,10 @@
 gcloud compute instances create es5-node-$1  \
        --image-project debian-cloud \
-       --image-family debian-10 \
+       --image-family debian-9 \
        --machine-type n1-highmem-8 \
        --zone europe-west1-d \
        --metadata-from-file startup-script=startup.sh \
        --boot-disk-size "1024" \
        --boot-disk-type "pd-ssd" --boot-disk-device-name "es5-node-ssd-$1" \
        --project open-targets-library \
-       --scopes default,storage-rw
+       --scopes default,storage-rw,compute-rw
