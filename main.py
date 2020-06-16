3653b6891871367effb0577b7b756df2780f4066
Cinzia Malangone <cinzia@ebi.ac.uk>
2020-06-16 14:38:01 +0100

    test

--- main.py
+++ main.py
@@ -382,20 +382,17 @@ def parse_article_info(article, publication):
 
         if e.tag == 'PublicationTypeList':
             pub_types = []
-            logging.warning("Author: "+ e.Author )
-            logging.error("before : "+ e.PublicationType)
-            try:
+            if "PublicationType" in e:
                 logging.warnings("prima del for:")
                 for pub_type in e.PublicationType:
-                    logging.error("dentro il for")
                     pub_types.append(pub_type.text)
                 logging.error("assign")
                 publication['pub_types'] = pub_types
-            except Exception:
-                logging.error("PublicationType"+ e.PublicationType)
-                logging.error("E.tag"+e.tag)
-                logging.error("e"+e.text)
-                logging.error("e"+ e)
+            else:
+                logging.warnings("Author:"+ e.Author)
+                logging.error("Cinzia publicationType not found")
+                exit(1)
+
 
         if e.tag == 'ELocationID' and e.attrib['EIdType'] == 'doi':
             publication['doi'] = e.text
