c4d65881043a1c91ed0d443057edd17bb6cff5cb
Cinzia Malangone <cinzia@ebi.ac.uk>
2020-06-17 11:17:25 +0100

    Change startup info for cluster

--- gcp-persistent-disk/startup.sh
+++ gcp-persistent-disk/startup.sh
@@ -68,7 +68,7 @@ discovery:
     hosts_provider: gce
     minimum_master_nodes: 2
 indices.store.throttle.max_bytes_per_sec: "200mb"
-cluster.name: library201911v7
+cluster.name: library202006v1
 node.name: ${HOSTNAME}
 network.host: 0.0.0.0
 http.port: 9200
